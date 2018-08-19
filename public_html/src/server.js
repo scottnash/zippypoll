0dea989d59a3ea571f4c1a9f4b68d39e2c44314d
Scott Nash <snash@kbsp.com>
2018-08-19 18:33:38 -0400

    rearranging docker files and making them work on older version of linux that is on my vps

---
+++ public_html/src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express()
+
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + './static/'));
+app.set('views', './src/views');
+
+
+const loadPage = (req,res)=> {
+  res.render('index', { title: 'Zippy Poll', message: 'Coming Soon' });
+}
+
+app.get('/*', loadPage );
+
+app.listen(8081, function () {
+  console.log('app listening on port 8081!')
+});
